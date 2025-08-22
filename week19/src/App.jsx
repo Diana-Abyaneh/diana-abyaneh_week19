532f8fdd4e145d82beea137ae1907d328ebc9a84
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-08-22 12:52:14 +0330

    Add routes to app.jsx

--- week19/src/App.jsx
+++ week19/src/App.jsx
@@ -1,11 +1,25 @@
-import SignupPage from "./pages/SignupPage"
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <SignupPage />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/signup" replace />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
